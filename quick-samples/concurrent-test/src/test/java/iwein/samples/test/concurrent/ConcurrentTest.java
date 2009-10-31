ca5ed8e2def77895d47ce7c4ecfa58df9250b77a
iwein <iwein@fuld.net>
2009-10-31 13:57:49 +0100

    first steps with gradle

---
+++ quick-samples/concurrent-test/src/test/java/iwein/samples/test/concurrent/ConcurrentTest.java
@@ -0,0 +1,21 @@
+package iwein.samples.test.concurrent;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+public class ConcurrentTest {
+
+    @Test
+    public void shouldGoThroughPipeline() {
+        //given
+        
+
+        //when
+
+
+        //verify
+
+    }
+}
