48bb5c1faf5f82b0882b232570985d146b216683
iwein <iwein@fuld.net>
2010-01-13 09:53:08 +0100

    added router-test sample with header value router experiment

---
+++ quick-samples/router-test/src/test/java/iwein/samples/spring/integration/HeaderValueRouterTest.java
@@ -0,0 +1,58 @@
+package iwein.samples.spring.integration;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.channel.PollableChannel;
+import org.springframework.integration.core.MessageChannel;
+import org.springframework.integration.message.MessageBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ *
+ */
+@ContextConfiguration("classpath:queueStrainer.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HeaderValueRouterTest {
+
+  @Autowired
+  @Qualifier("in")
+  MessageChannel in;
+
+  @Autowired
+  @Qualifier("routed")
+  PollableChannel routed;
+
+  @Autowired
+  @Qualifier("defaultOutput")
+  PollableChannel defaultOutput;
+
+  @Before
+  public void setup() {
+  }
+
+  @Test
+  public void shouldRouteMessageWithMappedHeader() {
+    in.send(MessageBuilder.withPayload("foo").setHeader("TO", "routed").build());
+    assertNotNull(routed.receive(0));
+  }
+
+  @Test
+  public void shouldRouteMessageWithoutHeader(){
+    in.send(MessageBuilder.withPayload("foo").build());
+    assertNotNull(defaultOutput.receive(0));    
+  }
+
+  @Test
+  @Ignore //this doesn't work because Spring Integration tries to resolve channel "something"
+  public void shouldRouteMessageWithUnmappedHeader(){
+    in.send(MessageBuilder.withPayload("foo").setHeader("TO", "something").build());
+    assertNotNull(defaultOutput.receive(0));
+  }
+}
