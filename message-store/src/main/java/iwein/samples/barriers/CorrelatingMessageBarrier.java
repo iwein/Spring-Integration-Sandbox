ad0e74ee600ac19b40b61c8f86f3dc4f8a057c28
iwein <iwein@fuld.net>
2010-05-31 16:42:57 +0200

    alternative solution using locking in the CorrelatingMessageBarrier

--- message-store/src/main/java/iwein/samples/barriers/CorrelatingMessageBarrier.java
+++ message-store/src/main/java/iwein/samples/barriers/CorrelatingMessageBarrier.java
@@ -10,6 +10,7 @@ import org.springframework.integration.store.MessageGroupStore;
 
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Iwein Fuld
@@ -17,6 +18,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 public class CorrelatingMessageBarrier implements MessageHandler, MessageSource {
 
   private Queue<Object> correlationsInReservoir = new ArrayBlockingQueue<Object>(10000);
+  private ConcurrentHashMap<Object, Object> correlationLocks = new ConcurrentHashMap<Object, Object>();
   private MessageGroupStore reservoir = new SimpleMessageStore(10000);
   private ReleaseStrategy releaseStrategy;
   private CorrelationStrategy correlationStrategy;
@@ -24,25 +26,40 @@ public class CorrelatingMessageBarrier implements MessageHandler, MessageSource 
   @Override
   public void handleMessage(Message<?> message) throws MessageRejectedException, MessageHandlingException, MessageDeliveryException {
     Object correlationKey = correlationStrategy.getCorrelationKey(message);
-    reservoir.addMessageToGroup(correlationKey, message);
-    correlationsInReservoir.offer(correlationKey);
-    System.out.println("handled message: "+ message);
+    Object lock = getLock(correlationKey);
+    synchronized (lock) {
+      reservoir.addMessageToGroup(correlationKey, message);
+      correlationsInReservoir.offer(correlationKey);
+    }
+    System.out.println("handled message: " + message);
   }
 
   @Override
   public Message receive() {
-    for (Object key : correlationsInReservoir) {
-      MessageGroup group = reservoir.getMessageGroup(key);
-      if (releaseStrategy.canRelease(group)) {
-        Message<?> nextMessage = group.getOne();
-        if(nextMessage==null){
-          correlationsInReservoir.remove(key);
-          reservoir.removeMessageGroup(key);
+    for (Object key : correlationLocks.keySet()) {
+      Object lock = getLock(key);
+      synchronized (lock) {
+        MessageGroup group = reservoir.getMessageGroup(key);
+        if (releaseStrategy.canRelease(group)) {
+          Message<?> nextMessage = group.getOne();
+          if (nextMessage == null) {
+            remove(key);
+          }
+          return nextMessage;
         }
-        return nextMessage;
       }
     }
     return null;
+  }
+
+  private void remove(Object key) {
+    correlationLocks.remove(key);
+    reservoir.removeMessageGroup(key);
+  }
+
+  private Object getLock(Object correlationKey) {
+    correlationLocks.putIfAbsent(correlationKey, correlationKey);
+    return correlationLocks.get(correlationKey);
   }
 
   public void setReleaseStrategy(ReleaseStrategy releaseStrategy) {
